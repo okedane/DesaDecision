da4b18f7e6bd8f1fdb4d6b3e802de84ce0a0cb62
okedane <romadani.code@gmail.com>
2026-01-06 18:27:55 +0700

    first commit

---
+++ database/migrations/2025_12_30_123555_create_pendaftaran_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pendaftaran', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pelamar_id')->constrained('pelamar')->onDelete('cascade');
+
+            $table->date('tanggal_daftar');
+            $table->enum('status', ['menunggu', 'lolos', 'tidak_lolos'])->default('menunggu');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pendaftaran');
+    }
+};
